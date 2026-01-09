ae75b36065d2ff0a0497f2161331ce9279d03065
imzicun <imamabd05@gmail.com>
2026-01-09 20:46:12 +0000

    Update complementary.py

--- src/complementary.py
+++ src/complementary.py
@@ -1,1 +1,9 @@
+def complementary_filter(theta_prev, rate, accel_theta, dt, alpha=0.98):
+    """
+    Complementary filter:
+      predict angle by integrating rate,
+      then blend with accelerometer angle.
+    """
+    pred = theta_prev + rate * dt
+    return alpha * pred + (1.0 - alpha) * accel_theta
 
